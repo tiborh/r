ec99b65963a0f23d90c40ee2fb377a6b5e293689
tibor harcsa <tibor@harcsa.net>
2016-07-15 00:25:54 +0200

    data import course has been finished.

--- data_import.r
+++ data_import.r
@@ -1,19 +1,4 @@
-clean.states <- function(df) {
-    df$name = as.character(df$name)
-    df$abbreviation = as.character(df$abbreviation)
-    df$assoc_press = as.character(df$assoc_press)
-    df$notes = as.character(df$notes)
-    return(df)
-}
-
-clean.states0 <- function(df) {
-    df <- df[-c(1,52,53),]
-    df$Sr.No <- as.integer(df$Sr.No)
-    df$Population.est. <- as.numeric(gsub(",","",df$Population.est.))
-    Sys.setlocale("LC_TIME","en_GB.utf8")
-    df$Statehood <- as.Date(df$Statehood,format="%B %d, %Y")
-    return(df)
-}
+source("clean_states.r")
 
 ## utils package (default)
 
