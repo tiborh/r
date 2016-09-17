df30db0076607a3d8c4d727b0f627059337d416f
Tibor Harcsa <tibor.harcsa@ericsson.com>
2016-09-17 18:22:19 +0200

    another way to the home folder

--- home_dir.r
+++ home_dir.r
@@ -1,5 +1,6 @@
 Sys.getenv()                            # the whole stuff
 home_directory <- Sys.getenv("HOME")    # /home/{userid}
-
-
-
+path.expand("~")                        # same
+file.path("~")                          # ~
+file.exists("~")                        # T
+file.exists(file.path("~","documents")) # T
