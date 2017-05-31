79b787277c475bab5acc414ed3f1473a03b63b2c
Tibor Harcsa <tibor.harcsa@ericsson.com>
2017-05-31 20:14:48 +0200

    making the example easier to see and extended to ggplot too

--- factor_order.r
+++ factor_order.r
@@ -1,9 +1,18 @@
-a <- data.frame(a = sample(letters[1:2],10,replace=T), b = sample(1:3,10,replace=T))
-b <- a
-c <- a
-b$a <- factor(b$a,levels=c("b","a"))    # the order can be determined this way
-c$a <- factor(c$a,levels=c("c","d"))    # NA's will appear
-a$a <- factor(a$a)
-str(a)
-str(b)
-str(c)
+a <- data.frame(col1 = sample(letters[1:2],10,replace=T), col2 = sample(LETTERS[1:3],10,replace=T))
+require(dplyr)
+require(ggplot2)
+require(pander)
+pander(a)
+sum.a <- a %>% group_by(col1,col2) %>% summarise(number=n())
+sum.a %>% ggplot(aes(col1,number)) + geom_bar(aes(colour=col2,fill=col2),stat="identity")
+
+sum.b <- sum.a
+
+str(sum.a)
+pander(sum.a)
+
+sum.b$col2 <- factor(sum.b$col2,levels=c("C","B","A"))
+
+str(sum.b)
+pander(sum.b)
+sum.b %>% ggplot(aes(col1,number)) + geom_bar(aes(colour=col2,fill=col2),stat="identity") # palette order is also opposite
