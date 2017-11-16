da7042b254a09a83ffcc0fa7e7148bfd50399f41
Tibor Harcsa <tibor.harcsa@ericsson.com>
2017-11-16 21:42:38 +0100

    some further refinement

--- comp_vectors.r
+++ comp_vectors.r
@@ -17,11 +17,27 @@ if (!file.exists(fp2))
 
 q <- scan(fp1,what=character())
 d <- scan(fp2,what=character())
+
 print(paste("from",fp1[length(fp1)],"to",fp2[length(fp2)]))
 first2second <- setdiff(q,d)
+## intersect12 <- intersect(q,d)
+
+print("Difference:")
 print(paste("length:",length(first2second)))
 print(first2second)
+
+## print("Intersection:")
+## print(paste("length:",length(intersect12)))
+## print(intersect12)
+
 print(paste("from",fp2[length(fp2)],"to",fp1[length(fp1)]))
 second2first <- setdiff(d,q)
+intersect21 <- intersect(d,q)
+
+print("Difference:")
 print(paste("length:",length(second2first)))
 print(second2first)
+
+print("Intersection:")
+print(paste("length:",length(intersect21)))
+print(intersect21)
