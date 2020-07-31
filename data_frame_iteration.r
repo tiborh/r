0c835600bea7aff4c9013fd243711cdbd1bf71a9
Tibor Harcsa <tibor.harcsa@ericsson.com>
2020-07-31 21:01:25 +0200

    better show the structure of what is passed

--- data_frame_iteration.r
+++ data_frame_iteration.r
@@ -6,13 +6,14 @@ cat("The data frame:\n")
 print(a)
 
 cat("plain for in df iteration goes column by column:\n")
-for (it in a) cat("item:",it,"\n")
+for (it in a) print(str(it))
 
 cat("row by row iteration breaks it down to mini data frames:\n")
-for (i in 1:nrow(a)) print(a[i,])
+for (i in 1:nrow(a)) print(str(a[i,]))
 
 cat("Best solution might be the use an apply function:\n")
 cat("by row:\n")
 apply(a,1,function(x) {print(str(x))})
 cat("by column:\n")
 apply(a,2,function(x) {print(str(x))})
+cat("But the coercion this brings may not be the result you want.\n")
