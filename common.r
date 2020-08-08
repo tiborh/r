3799f11b39acbd4b21874281b2d91ad8d95f6a2a
Tibor Harcsa <silurust@gmail.com>
2020-08-08 05:19:13 +0200

    merging common.r and script_env.r

--- common.r
+++ common.r
@@ -1,3 +1,22 @@
+DATA.DIR <- file.path(".","data")
+if (!dir.exists(DATA.DIR))
+    dir.create(DATA.DIR)
+IMG.DIR <- file.path(".","img")
+if (!dir.exists(IMG.DIR))
+    dir.create(IMG.DIR)
+
+get.raw.df <- function(loc.site,loc.file,force.remote=F,data.dir=DATA.DIR) {
+    remote.site <- paste0(loc.site,loc.file)
+    local.path <- file.path(data.dir,loc.file)
+    if (file.exists(local.path) && !force.remote)
+        raw.data <- read.csv(local.path)
+    else {
+        raw.data <- read.csv(remote.site)
+        write.csv(raw.data,file=local.path)
+    }
+    return(raw.data)
+}
+
 make.string.v <- function(num,maxStrLen=12) {
     v <- c()
     for (i in 1:num)
