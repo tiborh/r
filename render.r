8c80b8e3a8d09c5d34166ed242ea1beff4f9a4ff
Tibor Harcsa <tibor.harcsa@ericsson.com>
2022-09-05 13:37:40 +0200

    update to help automatic install, plus added mysterious mime package

--- render.r
+++ render.r
@@ -1,14 +1,20 @@
 #!/usr/bin/env Rscript
 
-if (!require(rmarkdown)) install.packages("rmarkdown")
-require(rmarkdown)
+UHOME <- Sys.getenv("HOME")
+RSHARED <- file.path(UHOME,"repos","r")
+COMMONR <- file.path(RSHARED,"common.r")
+source(COMMONR)
 
-args = commandArgs(trailingOnly=T)
-if (!length(args))
+check.packages(c("rmarkdown","mime"))
+
+if (NARGS < 1)
     stop("Rmd file to render is needed as argument.")
 
-fn <- args[1]
+fn <- ARGS[1]
 if (!file.exists(fn))
     stop("File does not exist: ",fn)
+
 rmarkdown::render(fn)
 rmarkdown::render(fn, pdf_document())
+##rmarkdown::render(fn, slidy_presentation())
+##rmarkdown::render(fn, beamer_presentation())
